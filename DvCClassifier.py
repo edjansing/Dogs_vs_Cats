33283192a6b259970ee28ad52e23b7a73f3b4900
Dave Jansing <david.jansing@omniearth.net>
2016-09-06 09:20:12 -0700

    First commit

---
+++ DvCClassifier.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+FILENAME:   DvCClassifier.py
+AUTHOR:     dave
+ORG:        OmniEarth, Inc.
+DATE:       9/6/16, 06:56
+VERSION:    0.01
+PY VER:     2.7.X
+
+Info:       This file defines the classifier that will be used for Dogs v Cat classification.
+
+Dependencies:
+            numpy
+            tensorflow
+
+Versioning:
+            0.01:  Initial version
+"""
+
+import numpy as np
+import tensorflow as tf
+
+# preProcessImagery:  A method to subtract the "VGG" mean from each pixel
+# Expects the images array to be 4-D (numberSamples, height, width, channels)
+def preProcessImagery(images):
+    vggMean = [123.68, 116.779, 103.939]
+    return images - np.tile(vggMean, (images.shape[0], images.shape[1], images.shape[2], 1))
+
+# maxPool2x2:  Performs a 2 x 2 max pooling on a tensor
+def maxPool2x2(inTensor):
+    return tf.nn.max_pool(
+        inTensor, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+# This defines the Convolutional Neural Network (CNN).  Based on example from Tensorflow/skflow
+# Github page.
+def conv_model(X, y):
+    # reshape X to 4d tensor with 2nd and 3rd dimensions being image width and
+    # height final dimension being the number of color channels.
+    X = tf.reshape(X, [-1, 224, 224, 3])
+    with tf.variable_scope('pre-process'):
+        X = preProcessImagery(X)
+
+    with tf.variable_scope('conv_layer1'):
+        h_conv1 = learn.ops.conv2d(X, n_filters=32, filter_shape=[5, 5],
+                               bias=True, activation=tf.nn.relu)
+        h_pool1 = max_pool_2x2(h_conv1)
+
+    with tf.variable_scope('conv_layer2'):
+        h_conv2 = learn.ops.conv2d(h_pool1, n_filters=64, filter_shape=[5, 5],
+                                   bias=True, activation=tf.nn.relu)
+        h_pool2 = max_pool_2x2(h_conv2)
+        # reshape tensor into a batch of vectors
+        h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
+
+    # densely connected layer with 1024 neurons.
+    h_fc1 = learn.ops.dnn(
+        h_pool2_flat, [1024], activation=tf.nn.relu, dropout=0.5)
+
+    return learn.models.logistic_regression(h_fc1, y)
---
+++ preprocImagery.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+FILENAME:   preprocImagery.py
+AUTHOR:     dave
+ORG:        OmniEarth, Inc.
+DATE:       9/5/16, 08:11
+VERSION:    0.01
+PY VER:     2.7.X
+
+Info:       Preprocess the dogs vs cats imagery into training/test bundles
+
+Dependencies:
+            skimage
+            numpy
+            glob
+            os.path
+            tqdm (a really cool progress bar)
+
+Versioning:
+            0.01:  Initial version
+"""
+
+import numpy as np
+from skimage.io import imread
+from skimage.transform import resize
+import matplotlib.pyplot as plt
+import glob
+from os.path import join
+from tqdm import tqdm
+
+def centerCrop(img):
+    # Borrowed from "VGG in TensorFlow · Davi Frossard"
+    image_h, image_w, _ = np.shape(img)
+    shorter_side = min(image_h, image_w)
+    scale = 224. / shorter_side
+    image_h, image_w = np.ceil([scale * image_h, scale * image_w]).astype('int32')
+    img = resize(img, (image_h, image_w))
+    crop_x = (image_w - 224) / 2
+    crop_y = (image_h - 224) / 2
+    img = img[crop_y:crop_y + 224,crop_x:crop_x + 224, :]
+
+    return img
+
+def returnFileList(inDir, pattName):
+    return glob.glob(join(inDir, pattName))
+
+if __name__ == '__main__':
+    dataDir = '/Users/dave/Data/DogsvCats/train'
+    dogFileList = returnFileList(dataDir, 'dog*')
+    catFileList = returnFileList(dataDir, 'cat*')
+    numberSamples = len(dogFileList) + len(catFileList)
+    print('Total number of samples = %d' % numberSamples)
+
+    # How much to hold out?
+    testSampleSize = np.ceil(numberSamples * 0.10)
+    print('Total number to hold out for test = %d' % testSampleSize)
+
+    # Allocate numpy arrays
+    images = np.zeros((numberSamples, 224, 224, 3), dtype='float32')
+    labels = np.zeros((numberSamples), dtype='float32')
+
+    # Get imagery, center crop, store in array
+    for k in tqdm(range(len(dogFileList))):
+        img = imread(dogFileList[k])
+        images[k, :, :, :] = centerCrop(img)
+        labels[k] = +1.0
+    for k in tqdm(range(len(catFileList))):
+        img = imread(catFileList[k])
+        images[k + len(dogFileList), :, :, :] = centerCrop(img)
+        labels[k] = 0.0
+
+    # Save off results
+    np.savez('/Users/dave/Data/DogsvCats/preprocImagery.npz', images=images, labels=labels)
